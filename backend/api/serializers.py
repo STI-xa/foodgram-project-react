d1dd6e0d8cda762c5f240e07bb8dad965ced5483
Полина Николаева <sti-xa@ya.ru>
2023-03-24 20:14:31 +0300

    fix1

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -97,7 +97,7 @@ class RecipeListSerializer(serializers.ModelSerializer):
             return False
         return Favourite.objects.filter(user=user, recipe=obj).exists()
 
-    def get_is_in_shopping_cart(self, obj):
+    def get_in_shopping_cart(self, obj):
         user = self.context.get('request').user
         if user.is_anonymous:
             return False
