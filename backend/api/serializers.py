023dafbcbda497cd0629457e5803feeb3934efeb
Полина Николаева <sti-xa@ya.ru>
2023-03-26 16:31:50 +0300

    fix5

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -43,7 +43,7 @@ class CustomUserSerializer(UserSerializer):
         user = self.context.get('request').user
         if user.is_anonymous:
             return False
-        return Subscribe.objects.select_related('author').exists()
+        return Subscribe.objects.filter(user=user, author=obj).exists()
 
 
 class IngredientSerializer(serializers.ModelSerializer):
